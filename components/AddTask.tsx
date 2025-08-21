fb9e9a00e662d899283a882dded4561b52830070
Henry Han <henry.han@usask.ca>
2025-08-20 21:15:44 -0400

    Implement the add new task Popup

--- components/AddTask.tsx
+++ components/AddTask.tsx
@@ -1,12 +1,20 @@
-import { Button } from "@heroui/button"
+"use client"
+
+import { Button } from "@heroui/react"
 import { AiOutlinePlus } from 'react-icons/ai'
+import Popup from "./Popup"
 
 const AddTask = () => {
+
+  const {isOpen, onOpen, onOpenChange} = Popup.useModal();
+
   return (
     <div>
-        <Button className="btn btn-primary" color="secondary">
+        <Button onPress={onOpen} className="btn btn-primary" color="secondary">
           Add new task <AiOutlinePlus size={16}/>
         </Button>
+
+        <Popup isOpen={isOpen} onOpenChange={onOpenChange}/>
     </div>
   )
 }
---
+++ components/Popup.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@heroui/react";
+
+const Popup = ({ isOpen, onOpenChange } : {isOpen: boolean, onOpenChange: () => void}) => {
+
+  return (
+    <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
+              <ModalBody>
+                <p></p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+    </Modal>
+  )
+}
+
+export default Popup
+Popup.useModal = useDisclosure;
