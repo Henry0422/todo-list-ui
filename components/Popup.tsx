4735e7a93ba4e548aa52ea129445f544a21dfd0a
Henry Han <henry.han@usask.ca>
2025-08-22 12:00:15 -0400

    Code formatting and cleanup in Popup.tsx

--- components/Popup.tsx
+++ components/Popup.tsx
@@ -12,7 +12,7 @@ import {
   DatePicker,
 } from "@heroui/react";
 import { getLocalTimeZone, today } from "@internationalized/date";
-import { useState } from "react"
+import { useState } from "react";
 
 interface PopupProps {
   isOpen: boolean;
@@ -28,87 +28,85 @@ const Popup: React.FC<PopupProps> = ({ isOpen, onOpenChange, listId, onTaskAdded
   
   const handleSubmitNewTodo = async () => {
     try {
-    const res = await fetch(`http://localhost:3001/tasks`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        title: titleValue,
-        description: descriptionValue,
-        dueDate: new Date(),
-        listId: listId+1
-      })
-    });
+      const res = await fetch(`http://localhost:3001/tasks`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title: titleValue,
+          description: descriptionValue,
+          dueDate: new Date(),
+          listId: listId+1
+        })
+      });
 
-    const json = await res.json();
+      const json = await res.json();
 
-    // update parent
-    onTaskAdded(json);
+      // update parent
+      onTaskAdded(json);
 
-    // reset fields
-    setTitleValue("");
-    setDescriptionValue("");
+      // reset fields
+      setTitleValue("");
+      setDescriptionValue("");
 
-    // close modal
-    onOpenChange(false);
-  } catch (err) {
-    console.error('Error creating task:', err);
-  }
-    setTitleValue("");
+      // close modal
+      onOpenChange(false);
+    } catch (err) {
+      console.error('Error creating task:', err);
+    }
   }
 
   return (
     <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
-              <ModalBody>
-                <h3 className="font-bold text-lg">Add new task</h3>
-                <div className="modal-action">
-                  <Input
-                    isRequired
-                    label="Title"
-                    name="title"
-                    placeholder="Enter a title"
-                    value={titleValue}
-                    onChange={e => setTitleValue(e.target.value)}
-                  />
-                  <Input
-                    label="Description"
-                    name="description"
-                    placeholder="Optional"
-                    value={descriptionValue}
-                    onChange={e => setDescriptionValue(e.target.value)}
-                  />
-                  <DatePicker
-                    hideTimeZone
-                    isRequired
-                    showMonthAndYearPickers
-                    label="Due Date"
-                    value={dueDateValue}
-                    onChange={(date) => {
-                      if (date)
-                        setDueDateValue(date);
-                    }}
-                  />
-                </div>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={handleSubmitNewTodo}>
-                  Submit
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">Add new task</ModalHeader>
+            <ModalBody>
+              <div className="modal-action">
+                <Input
+                  isRequired
+                  label="Title"
+                  name="title"
+                  placeholder="Enter a title"
+                  value={titleValue}
+                  onChange={e => setTitleValue(e.target.value)}
+                />
+                <Input
+                  label="Description"
+                  name="description"
+                  placeholder="Optional"
+                  value={descriptionValue}
+                  onChange={e => setDescriptionValue(e.target.value)}
+                />
+                <DatePicker
+                  hideTimeZone
+                  isRequired
+                  showMonthAndYearPickers
+                  label="Due Date"
+                  value={dueDateValue}
+                  onChange={(date) => {
+                    if (date)
+                      setDueDateValue(date);
+                  }}
+                />
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="danger" variant="light" onPress={onClose}>
+                Close
+              </Button>
+              <Button color="primary" onPress={handleSubmitNewTodo}>
+                Submit
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
     </Modal>
-  )
-}
+  );
+};
 
 export default Popup;
 export const usePopup = useDisclosure;
